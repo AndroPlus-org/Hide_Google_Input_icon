8d20152cf4d82ba6e720d80116eff8072dcde88e
Noumi Ryoko <hosigumayuugi@gmail.com>
2014-02-17 14:02:08 +0900

    1st

---
+++ src/com/wedy/googleinputh/NotificationiconPatcher.java
@@ -0,0 +1,27 @@
+package com.wedy.googleinputh;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class NotificationiconPatcher implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.google.android.inputmethod.japanese")) {
+            ClassLoader classLoader = lpparam.classLoader;
+            XC_MethodReplacement methodreplacer = new XC_MethodReplacement() {
+                protected Object replaceHookedMethod(
+                        XC_MethodHook.MethodHookParam paramAnonymousMethodHookParam)
+                        throws Throwable {
+                    return null;
+                }
+            };
+            XposedHelpers.findAndHookMethod("com.google.android.inputmethod.japanese.MozcService",
+                    classLoader, "bm", methodreplacer);
+        }
+    }
+
+}
